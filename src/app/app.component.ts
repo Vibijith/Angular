14b6a0a8a43c4f6593860d6474002cf610797936
Vibijith <vibijithkongat@gmail.com>
2024-07-26 10:26:45 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardsComponent } from './ui/cards/cards.component';
 import { RouterOutlet } from '@angular/router';
+import { CardListComponent } from './ui/card-list/card-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  CardsComponent, CardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent{
+  title = 'prodcut-app'
 }
